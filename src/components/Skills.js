426736c0a176227db2e6641a8cdc787f823fcee2
ksburhan <burhan.koeseler@hotmail.de>
2024-01-17 21:00:27 +0900

    update components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,33 @@
+import { CpuChipIcon } from "@heroicons/react/24/solid";
 import React from "react";
+import { skills } from "../data";
 
 const Skills = () => {
-    return (<h1>Skills</h1>);
+    return (
+        <section id="skills">
+          <div className="container px-5 py-10 mx-auto">
+            <div className="text-center mb-20">
+              <CpuChipIcon className="w-10 inline-block mb-4" />
+              <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+                Skills &amp; Technologies
+              </h1>
+              <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+              </p>
+            </div>
+            <ul>
+            <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+
+              {skills.map((skill) => (
+                    <li key={skill.id}>
+                      <img src={skill.img} alt="skill-icon" />
+                    </li>
+              ))}
+            </div>
+
+            </ul>
+          </div>
+        </section>
+      );
 }
 
 export default Skills;
--- src/data.js
+++ src/data.js
@@ -1,5 +1,21 @@
 export const projects = [];
 
-export const skills = [];
+export const skills = [{
+    img: "https://skillicons.dev/icons?i=html,css",
+    id: 1,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=js,ts",
+    id: 2,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=react,next",
+    id: 3,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=tailwind,scss",
+    id: 4,
+  },
+];
 
 export const testimonials = [];
