479740fef575af6514445b764bd6e8a67aaa5503
ksburhan <burhan.koeseler@hotmail.de>
2024-01-22 19:17:13 +0900

    add project dummy information

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
-export const projects = [];
+export const projects = [
+  {
+    link: "https://fliegengitterworld.de",
+    image: "https://preview.redd.it/minish-cap-toon-link-mod-is-now-in-development-v0-iogndtlilk6c1.png?width=830&format=png&auto=webp&s=7a2b3044b4713620fdc099b250d3b8f39b96b503",
+    subtitle: "Fliegengitter World",
+    description: "Homepage for local businuess in Augsburg, Germany."
+  },
+];
 
-export const skills = [{
+export const skills = [
+  {
     img: "https://skillicons.dev/icons?i=html,css",
     id: 1,
   },
