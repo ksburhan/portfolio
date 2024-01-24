750d178b4e2b626faa5e9ba555480874f47b6ea9
ksburhan <burhan.koeseler@hotmail.de>
2024-01-24 16:15:08 +0900

    adjust background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <section id="contact" className="relative">
+        <section id="contact" className="relative bg-white">
           <div className="container max-w-7xl px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
               <div className="relative flex flex-wrap py-6 rounded shadow-md">
                 <div className="lg:w-1/2 px-6">
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,7 +4,7 @@ import { testimonials } from "../data";
 
 const Testimonials = () => {
     return (
-        <section id="testimonials" className="bg-white">
+        <section id="testimonials">
           <div className="container max-w-7xl px-5 py-10 mx-auto text-center">
             <UsersIcon className="w-10 inline-block mb-4" />
             <h1 className="sm:text-4xl text-3xl font-medium title-font  mb-12">
