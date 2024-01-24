1fb325a025f448269200aabd2c2a963ba1d5022b
ksburhan <burhan.koeseler@hotmail.de>
2024-01-24 15:48:45 +0900

    update about styling

--- src/components/About.js
+++ src/components/About.js
@@ -11,40 +11,33 @@ const About = () => {
               Hi, I'm Burhan Köseler.
                 <br className="hidden lg:inline-block" />A passionate Full-Stack Engineer based in Japan. 📍
               </h1>
-              <p className="mb-8 leading-relaxed">
+              <p className="mb-4 leading-relaxed">
                 Hey, my name is Burhan, and I'm a Full-Stack Developer. My passion is to create and develop clean Services with clean Websites for my users.
                 My main stack currently is React/Next.js in combination with Tailwind CSS and TypeScript.
               </p>
               <div className="flex justify-center">
                 <a
-                  href="#contact"
-                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                  Work With Me
+                  className="pr-1"
+                  aria-label="linkedin"
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://www.linkedin.com/in/burhan-k%C3%B6seler-2b4130235/"
+                >
+                  <IconBrandLinkedin width={32} height={32} />
                 </a>
                 <a
-                  href="#projects"
-                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                  See My Past Work
+                  className="pl-1"
+                  aria-label="github"
+                  rel="noreferrer"
+                  target="_blank"
+                  href="https://github.com/ksburhan"
+                >
+                  <IconBrandGithub width={32} height={32} />
                 </a>
               </div>
               <span className="flex justify-center">
-                  <a
-                    aria-label="linkedin"
-                    rel="noreferrer"
-                    target="_blank"
-                    href="https://www.linkedin.com/in/burhan-k%C3%B6seler-2b4130235/"
-                  >
-                    <IconBrandLinkedin width={32} height={32} />
-                  </a>
-                  <a
-                    aria-label="github"
-                    rel="noreferrer"
-                    target="_blank"
-                    href="https://github.com/ksburhan"
-                  >
-                    <IconBrandGithub width={32} height={32} />
-                  </a>
-                </span>
+                  
+              </span>
             </div>
             <div className="hero-img lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
           </div>
