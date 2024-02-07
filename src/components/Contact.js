dcfae9f86e9a7c43454434a0bd291342ae0d3713
ksburhan <burhan.koeseler@hotmail.de>
2024-02-07 20:05:55 +0900

    update contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,81 +1,115 @@
 import React from "react";
+import {FC, memo, useCallback, useMemo, useState} from 'react';
+import { EnvelopeIcon } from "@heroicons/react/24/solid";
+
+import { contacts } from "../data";
 
 const Contact = () => {
+  const defaultData = useMemo(
+    () => ({
+      name: '',
+      email: '',
+      message: '',
+    }),
+    [],
+  );
+  const [data, setData] = useState(defaultData);
+
+  const onChange = useCallback((event) => {
+      const {name, value} = event.target;
+
+      const fieldData = {[name]: value};
+
+      setData({...data, ...fieldData});
+    },
+    [data],
+  );
+  const handleSendMessage = useCallback(
+    async (event) => {
+      event.preventDefault();
+      /**
+       * This is a good starting point to wire up your form submission logic
+       * */
+      console.log('Data to send: ', data);
+    },
+    [data],
+  );
+  const inputClasses =
+    'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
+
     return (
-        <section id="contact" className="relative bg-white">
-          <div className="container max-w-7xl px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-              <div className="relative flex flex-wrap py-6 rounded shadow-md">
-                <div className="lg:w-1/2 px-6">
-                  <h2 className="title-font font-semibold  tracking-widest text-xs">
-                    LOCATION
-                  </h2>
-                  <p className="mt-1">
-                    Japan
-                  </p>
-                </div>
-                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                  <h2 className="title-font font-semibold  tracking-widest text-xs">
-                    EMAIL
-                  </h2>
-                  <a className="text-indigo-400 leading-relaxed" href="mailto:burhan.koeseler@hotmail.de">
-                    burhan.koeseler@hotmail.de
-                  </a>
-                  <h2 className="title-font font-semibold  tracking-widest text-xs mt-4">
-                    PHONE
-                  </h2>
-                  <p className="leading-relaxed">+81 80 7409 7126</p>
-                </div>
-              </div>
-            </div>
-            <form
-              netlify="true"
-              name="contact"
-              className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-              <h2 className=" sm:text-4xl text-3xl mb-1 font-medium title-font">
-                Hire Me
-              </h2>
-              <p className="leading-relaxed mb-5">
-              </p>
-              <div className="relative mb-4">
-                <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label
-                  htmlFor="message"
-                  className="leading-7 text-sm text-gray-400">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <button
-                type="submit"
-                className=" bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Submit
-              </button>
-            </form>
+        <section id="contact" className="px-4 py-16 md:py-24 lg:px-8">
+          <div className='mx-auto max-w-screen-lg'>
+        <div className="flex flex-col gap-y-6">
+          <div className="flex flex-col gap-6 md:flex-row md:items-center">
+          <EnvelopeIcon className="hidden h-16 w-16 text-white md:block" />
+          <h2 className="text-2xl font-bold text-white">Get in touch</h2>
+        </div>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          <div className="order-2 col-span-1 md:order-1 ">
+          <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
+      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+      <input
+        autoComplete="email"
+        className={inputClasses}
+        name="email"
+        onChange={onChange}
+        placeholder="Email"
+        required
+        type="email"
+      />
+      <textarea
+        className={inputClasses}
+        maxLength={250}
+        name="message"
+        onChange={onChange}
+        placeholder="Message"
+        required
+        rows={6}
+      />
+      <button
+        aria-label="Submit contact form"
+        className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
+        type="submit">
+        Send Message
+      </button>
+    </form>
+          </div>
+          <div className="order-1 col-span-1 flex flex-col gap-y-4 md:order-2">
+            <p className="prose leading-6 text-neutral-300">Feel free to send me a mail or contact me over these socials!</p>
+            <dl className="flex flex-col space-y-4 text-base text-neutral-500 sm:space-y-2">
+            <div key="Instagram">
+                    <dt className="sr-only">Instagram</dt>
+                    <dd className="flex items-center">
+                      <a
+                        className=
+                          '-m-2 flex rounded-md p-2 text-neutral-300 hover:text-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500'
+                        href='instagram.com/ks_burhan'
+                        target="_blank">
+                        <span className="ml-3 text-sm sm:text-base">@ks_burhan</span>
+                      </a>
+                    </dd>
+                  </div>
+              { contacts.map(({type, text, href}) => {
+                return (
+                  <div key={type}>
+                    <dt className="sr-only">{type}</dt>
+                    <dd className="flex items-center">
+                      <a
+                        href={href}
+                        target="_blank">
+                        {/* <Icon aria-hidden="true" className="h-4 w-4 flex-shrink-0 text-neutral-100 sm:h-5 sm:w-5" /> */}
+                        <span className="ml-3 text-sm sm:text-base">{text}</span>
+                      </a>
+                    </dd>
+                  </div>
+                );
+              })}
+            </dl>
+          </div>
+        </div>
+      </div>
+      </div>
         </section>
       );
 }
