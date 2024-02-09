7e0386ff48e226e941a8746a3511461ae771031b
ksburhan <burhan.koeseler@hotmail.de>
2024-02-09 18:37:33 +0900

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,7 +35,7 @@ const Contact = () => {
     [data],
   );
   const inputClasses =
-    'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
+    'bg-neutral-500 pl-2 border-0 rounded-md placeholder:text-neutral-400 placeholder:text-md text-neutral-200 text-md resize-none';
 
   return (
     <section id="contact" className="px-4 py-16 md:py-24 lg:px-8">
@@ -48,7 +48,14 @@ const Contact = () => {
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             <div className="order-2 col-span-1 md:order-1 ">
               <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-                <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+                <input
+                  className={inputClasses}
+                  name="name"
+                  onChange={onChange}
+                  placeholder="Name"
+                  required
+                  type="text"
+                />
                 <input
                   autoComplete="email"
                   className={inputClasses}
@@ -60,7 +67,7 @@ const Contact = () => {
                 />
                 <textarea
                   className={inputClasses}
-                  maxLength={250}
+                  maxLength={368}
                   name="message"
                   onChange={onChange}
                   placeholder="Message"
@@ -69,33 +76,24 @@ const Contact = () => {
                 />
                 <button
                   aria-label="Submit contact form"
-                  className="w-max rounded bg-sky-300 px-4 py-2 text-sm font-medium shadow-md outline-none hover:bg-stone-800"
+                  className="w-max rounded bg-blue-300 px-4 py-2 text-md font-medium shadow-md outline-none hover:bg-blue-700"
                   type="submit">
                   Send Message
                 </button>
               </form>
             </div>
             <div className="order-1 col-span-1 flex flex-col gap-y-4 md:order-2">
-              <p className="prose leading-6 text-neutral-300">Feel free to send me a mail or contact me over these socials!</p>
-              <dl className="flex flex-col space-y-4 text-base text-neutral-500 sm:space-y-2">
-                <div key="Instagram">
-                  <dt className="sr-only">Instagram</dt>
-                  <dd className="flex items-center">
-                    <a
-                      className=
-                      '-m-2 flex rounded-md p-2 text-neutral-300 hover:text-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-500'
-                      href='instagram.com/ks_burhan'
-                      target="_blank">
-                      <span className="ml-3 text-sm sm:text-base">@ks_burhan</span>
-                    </a>
-                  </dd>
-                </div>
+              <p className="prose leading-6">Feel free to send me a mail or contact me over these socials!</p>
+              <dl className="flex flex-col space-y-4 text-base sm:space-y-2">
+
                 {contacts.map(({ type, text, href }) => {
                   return (
                     <div key={type}>
                       <dt className="sr-only">{type}</dt>
                       <dd className="flex items-center">
                         <a
+                          className=
+                          '-m-2 flex rounded-md p-2 hover:text-sky-300'
                           href={href}>
                           {/* <Icon aria-hidden="true" className="h-4 w-4 flex-shrink-0 text-neutral-100 sm:h-5 sm:w-5" /> */}
                           <span className="ml-3 text-sm sm:text-base">{text}</span>
