2bb2e3bfe644384e643ce842b6f58dbfa69836ed
ksburhan <burhan.koeseler@hotmail.de>
2024-02-27 18:48:57 +0900

    fi manifest errors

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,27 +1,22 @@
-import {NextPage} from 'next';
+import { NextPage } from 'next';
 import Head from 'next/head';
-import {useRouter} from 'next/router';
-import {memo, PropsWithChildren} from 'react';
+import { memo, PropsWithChildren } from 'react';
 import { Metadata } from '../../data/dataDef';
 
-const Page: NextPage<PropsWithChildren<Metadata>> = memo(({children, title, description}) => {
-  const {asPath: pathname} = useRouter();
-
+const Page: NextPage<PropsWithChildren<Metadata>> = memo(({ children, title, description }) => {
   return (
     <>
       <Head>
-      <meta charSet="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <meta
-      name="description"
-      content={description}
-    />
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-   
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content={description}
+        />
+
+        <link rel="manifest" href="manifest.json" />
+        <title>{title}</title>
       </Head>
       {children}
     </>
