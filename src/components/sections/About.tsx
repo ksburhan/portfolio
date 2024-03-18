7215aeaf859a3008ab700b02f552249942e4262f
ksburhan <burhan.koeseler@hotmail.de>
2024-03-18 22:59:14 +0900

    fix background size

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -9,7 +9,7 @@ import Section from "../layout/Section";
 const About = () => {
   const { name, header, description, imageSrc } = about;
   return (
-    <Section className="bg-gray-100 h-screen md:h-[calc(100vh-72px)]" sectionId={SectionId.About}>
+    <Section className="bg-gray-100" sectionId={SectionId.About}>
       <div className={'md:py-12 flex flex-col-reverse md:grid md:grid-cols-3'}>
         <div className={'text-center md:text-left md:pr-12 col-span-1 flex flex-col md:col-span-2'}>
           <div className="flex flex-col">
