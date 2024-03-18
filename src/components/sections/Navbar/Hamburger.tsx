f0e2fdf820d64852456c8abaf62a0ec1b3d62774
ksburhan <burhan.koeseler@hotmail.de>
2024-03-18 22:51:09 +0900

    update hamburger styling

--- src/components/sections/Navbar/Hamburger.tsx
+++ src/components/sections/Navbar/Hamburger.tsx
@@ -3,9 +3,11 @@ import classNames from 'classnames';
 import { navbarItems } from "../../../data/data";
 
 const Hamburger: FC<PropsWithChildren<{ isOpen: boolean }>> = ({ isOpen }) => {
+    const button = classNames("w-8 h-8 flex justify-around flex-nowrap flex-col",
+        "z-10 transition-all duration-300 ease-linear", isOpen ? "translate-x-1" : "");
     const slideMenu = classNames(isOpen ? "flex" : "hidden", "flex flex-col",
         "w-10/12 h-screen z-[300] bg-white fixed top-0 left-0 text-center text-base",
-        "items-center justify-center transition-all duration-300 ease-linear");
+        "items-center justify-center");
     const baseBurger = "w-8 h-1 rounded-lg bg-black origin-[1px] transition-all duration-300 ease-linear";
     const burger1 = classNames(baseBurger, isOpen ? "rotate-45" : "rotate-0");
     const burger2 = classNames(baseBurger, isOpen ? "translate-x-full opacity-0" : "translate-x-0 opacity-1");
@@ -14,12 +16,12 @@ const Hamburger: FC<PropsWithChildren<{ isOpen: boolean }>> = ({ isOpen }) => {
         <>
             <nav className={classNames(slideMenu)}>
                 {navbarItems.map(item => (
-                    <a key={item.name} href={item.href} className="mr-5 hover:text-sky-300">
+                    <a key={item.name} href={item.href} className="mr-5 text-4xl p-4 hover:text-sky-300">
                         {item.name}
                     </a>
                 ))}
             </nav>
-            <div className="w-8 h-8 flex justify-around flex-nowrap flex-col z-10">
+            <div className={button}>
                 <div className={classNames(burger1)} />
                 <div className={classNames(burger2)} />
                 <div className={classNames(burger3)} />
