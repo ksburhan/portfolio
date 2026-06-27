df8f5deba01e1243b2b0f6d5bc7ad98aaca1e84c
ksburhan <ksburhan@users.noreply.github.com>
2026-06-27 07:31:29 +0000

    deploy: 79b001ac3cd0c4d5733037e0b2dee63b06bac358

--- _next/static/v4ZcODhWE-gaW8EUy0rwi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/189-bd3875c5ee978f73.js","static/chunks/pages/index-307d80528f1f93aa.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
